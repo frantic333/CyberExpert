ec67efb4485f6f0db214f8cf9a26a97dc384c547
max <frantic33@yandex.ru>
2023-12-02 01:42:51 +0300

    Start project

---
+++ cyber_expert/cyber_expert/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cyber_expert project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cyber_expert.settings')
+
+application = get_wsgi_application()
