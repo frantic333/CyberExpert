3242975b2464bd82de9fdc14e846a33242d378f5
max <frantic33@yandex.ru>
2023-12-02 18:39:36 +0300

    Corrected auth_app/admin.py, protection/admin.py

--- cyber_expert/protection/function.py
+++ cyber_expert/protection/function.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 
 def tool_file_size(file):
-    if file.size >= 100 * 1024 * 1024:
+    if file.size > 100 * 1024 * 1024:
         raise ValidationError(message='Размер файла не должен превышать 100 Mb')
